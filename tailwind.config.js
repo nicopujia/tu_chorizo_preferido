44c2131bb1464b71690c7b201b23565a1abc9fa5
Nico Pujia <nicobatman0@gmail.com>
2023-10-16 13:13:30 -0300

    Add about and contact section and some other improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       fontFamily: {
         kanit: ["Kanit", "sans"],
       },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
     },
   },
   plugins: [],
